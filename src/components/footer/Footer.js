36af7f9a3f01e4b3de8758612fea85a8b37a7514
alejandracapis <acapistran17@gmail.com>
2024-11-22 17:57:35 -0600

    adjusting videos

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
             <copyright className='copyright'>
                 &copy;2024 Grupo Crusa. Todos los derechos reservados
             </ copyright>
-            <a href = {require("./avisodeprivacidad.pdf")}>Aviso de Privacidad</a>
+            <a className="aviso" href = {require("./avisodeprivacidad.pdf")}>Aviso de Privacidad</a>
         </div>
     );
 }
