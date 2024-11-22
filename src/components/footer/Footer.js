5cb86997b5429d9356d39b50ef15a6a6fbd4a34d
alejandracapis <acapistran17@gmail.com>
2024-11-22 16:28:56 -0600

    .

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <div className='footer-container'>        
+        <div className='footer-container'>
             <copyright className='copyright'>
                 &copy;2024 Grupo Crusa. Todos los derechos reservados
             </ copyright>
