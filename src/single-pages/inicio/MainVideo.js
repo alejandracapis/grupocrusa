ecd5606d23db6095ff19b0850bcf468e0fbc21bc
alejandracapis <acapistran17@gmail.com>
2024-12-03 14:07:05 -0600

    3 dic 2024 handling inline video

--- src/single-pages/inicio/MainVideo.js
+++ src/single-pages/inicio/MainVideo.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const MainVideo = () => {
+  const fallback = (video) =>
+{
+  var img = video.querySelector("img");
+  if (img)
+    video.parentNode.replaceChild(img, video);
+}
+
   return (
     <div className="one-video">
       <video
+      controls
+      autoPlay
+      loop
+      muted
+      playsInline
+      preload='auto'
+      className="one-video">
+        <source src={require('./bgvideo-cropped.mp4')} />
+          <source src={require('./bgvideo-cropped.mp4')} onerror={fallback} />
+            <img src={require('./bgvideo-cropped.gif')} alt='main gif'/>
+      </video>
+
+            {/* <video
         autoPlay
         muted={true}
         loop
@@ -13,7 +33,7 @@ const MainVideo = () => {
         mediaPlaybackRequiresUserActions={false}
         className="one-video">
         <source src={require('./bgvideo-cropped.mp4')} type="video/mp4" />
-      </video>
+      </video> */}
     </div>
   )
 }
