9e4057d2acc6710b6cc13194d2fcec61ad06111c
alejandracapis <acapistran17@gmail.com>
2024-12-03 14:45:59 -0600

    3 dic 2024 same

--- src/single-pages/inicio/MainVideo.js
+++ src/single-pages/inicio/MainVideo.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const MainVideo = () => {
+  const fallback = (video) => {
+    var img = video.querySelector("img");
+    if (img)
+      video.parentNode.replaceChild(img, video);
+  }
+
   function scrollDown() {
     document.getElementById("item-two").scrollIntoView({ behavior: 'smooth' });
 }
@@ -8,21 +14,14 @@ const MainVideo = () => {
   return (
     <section className="item-one">
       <video
-        controls
         autoPlay
         loop
-        muted='true'
-        playsInline='true'
-        preload='none'
+        muted
+        playsInline
+        preload='auto'
         className="one-video">
         <source src={require('./bgvideo-cropped.mp4')} />
-        <source src={require('./bgvideo-cropped.mp4')} 
-        onError={(video) => {
-          var img = video.querySelector("img");
-          if (img)
-            video.parentNode.replaceChild(img, video);
-          }} 
-          />
+        <source src={require('./bgvideo-cropped.mp4')} onerror={fallback} />
         <img src={require('./bgvideo-cropped.gif')} alt='main gif' />
       </video>
       <span className="one-head">
